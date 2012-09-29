5794254bef26d57425cee490d3c02682a1ae2c4e
Alexei Boev <alex@maximumweb.ru>
2012-09-30 00:49:15 +0400

    first good ajax onkeyup

--- tinymcetest.php
+++ tinymcetest.php
@@ -16,29 +16,36 @@
 		theme_advanced_buttons2 : "",
 		theme_advanced_buttons3 : "",
 		setup : function(ed) {
-	      ed.onKeyUp.add(function(ed, e) {
-
-	      });
-	   }
+			ed.onKeyUp.add(function(ed, e) {
+				var editorInstance = $( this ).attr('id');
+				saveNoteContents( $( '#'+editorInstance ) );
+			});
+		}
 	});
+	
+	function saveNoteContents( object ) {
+		var savingNote = object.parents().children('.note');
+		var savingNoteID = savingNote.attr('id');
+		var savingNoteCoords = savingNote.parents().offset();
+		var savingNoteCoords_x = savingNoteCoords.left;
+		var savingNoteCoords_y = savingNoteCoords.top;
+		var savingNoteContent = tinyMCE.get('somesortof').getContent();
+		$.post("/savenotecontents.php", {
+				noteID: savingNoteID,
+				noteContent: savingNoteContent,
+				coord_x: savingNoteCoords_x,
+				coord_y: savingNoteCoords_y
+			},
+			function(data) {
+				alert(data);
+			}
+		);
+	}
+	
 	$( function() {
 		$( '.draggable' ).draggable().resizable();
 		$( '#saveButton' ).click( function() {
-			var savingNote = $( this ).parents().children('.note');
-			var savingNoteID = savingNote.attr('id');
-			var savingNoteCoords = savingNote.parents().offset();
-			var savingNoteCoords_x = savingNoteCoords.left;
-			var savingNoteCoords_y = savingNoteCoords.top;
-			var savingNoteContent = tinyMCE.get('somesortof').getContent();
-			$.post("/savenotecontents.php", {
-					noteID: savingNoteID,
-					noteContent: savingNoteContent,
-					coords: savingNoteCoords_x + " " + savingNoteCoords_y
-				},
-				function(data) {
-					alert(data);
-				}
-			);
+			saveNoteContents( $( this ) );
 		});
 	});
 </script>
