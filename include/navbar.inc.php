1abc78bd7eaf07afaa82e2e459cd4f804c1c3809
Alexei Boev <alex@maximumweb.ru>
2012-10-06 03:44:06 +0400

    fixed navbar

--- include/navbar.inc.php
+++ include/navbar.inc.php
@@ -1,14 +1,16 @@
 <div class="topbar">
 	<ul class="navigation">
-		<li><a href="/">В начало</a>
+		<li>
 			<?
 				if( isset($_GET['category']) ) {
 					$sql = mysql_query("SELECT `notetype_name` FROM `notes_notetypes` WHERE `notetype_userid` = '".$_COOKIE['userid']."' AND `notetype_id` = '".$_GET['category']."' LIMIT 1");
 					if( $sql ) {
 						while( $row = mysql_fetch_array( $sql, MYSQL_ASSOC ) ) {
-							echo " / <span>".$row['notetype_name']."</span>";
+							echo "<a href=\"/\">Блокноты</a> / <span>".$row['notetype_name']."</span>";
 						}
 					}
+				} else {
+					echo "<span>Блокноты</span>";
 				}
 			?>
 		</li>
